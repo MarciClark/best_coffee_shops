1bb5a8e1ee559acae7da9c9b8433b45e5cc1ffa5
Marci Clark <marcig612@gmail.com>
2020-02-14 13:39:14 +0000

    added pry

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,6 +1,8 @@
 require_relative "best_coffee_shops/version"
 require_relative "best_coffee_shops/cli"
 
+require 'pry'
+
 module BestCoffeeShops
   class Error < StandardError; end
   # Your code goes here...

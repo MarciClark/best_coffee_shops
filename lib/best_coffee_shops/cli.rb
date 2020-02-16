1b03c2c9e10c12ae6b3df1dd1be5136694f13a0b
Marci Clark <marcig612@gmail.com>
2020-02-16 04:11:48 +0000

    added methods

--- lib/best_coffee_shops/cli.rb
+++ lib/best_coffee_shops/cli.rb
@@ -1,6 +1,13 @@
 class BestCoffeeShops::CLI 
   
   def call
-    puts "Coffee, coffee, coffee!  Here are Cincinnati's best"
+    puts "Coffee, coffee, coffee!  Here are Cincinnati's best coffee shops!"
   end 
+  
+  def list_shops
+  end 
+  
+  def get_shops_info
+  end 
+  
 end

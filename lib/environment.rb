99bdbd2c63a5728adc91f5b9dbf7dfb3b42be2aa
Marci Clark <marcig612@gmail.com>
2020-02-19 18:59:22 +0000

    added scraper folder and updated requirements

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,7 +1,9 @@
 require_relative "./best_coffee_shops/version"
 require_relative "./best_coffee_shops/cli"
+require_relative "./best_coffee_shops/scraper"
 
 require 'pry'
+require 'nokogiri'
 
 module BestCoffeeShops
   class Error < StandardError; end
